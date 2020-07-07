7b7517aa128ed6e6cea725366969002bc5de7f35
iwfan <i.wangfancn@gmail.com>
2020-07-07 22:51:16 +0800

    add vim

--- main.js
+++
@@ -1,9 +0,0 @@
-import { a } from 'test';
-import Vue from 'vue';
-console.log(a);
-
-console.log(Vue);
-
-importShim('vue').then(data => {
-  console.log(" == ", data);
-})
--- src/test.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-
-export const a = Vue;
-
